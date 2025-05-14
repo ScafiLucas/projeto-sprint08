931f8f9d9e8e0ae856787c1b0a99a949f6b1d9c7
ScafiLucas <scafilucas@gmail.com>
2025-05-14 16:54:33 -0300

    solucao sprint 08

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 # data.py
 
 # URL do servidor Urban Routes
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-04fc1d28-d55c-4dd5-9341-0374341902ad.containerhub.tripleten-services.com?lng=pt'
 
 # Dados de teste utilizados nos formulários
 ADDRESS_FROM = 'East 2nd Street, 601'
